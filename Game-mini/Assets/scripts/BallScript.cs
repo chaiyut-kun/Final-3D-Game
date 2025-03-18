b589398a984f8140a64a0c1ced2494dc62f6a453
chaiyut-kun <chwork28@hotmail.com>
2025-03-18 15:16:25 +0700

    Update Script for ball

--- Game-mini/Assets/scripts/BallScript.cs
+++ Game-mini/Assets/scripts/BallScript.cs
@@ -12,8 +12,11 @@ public class BallScript : MonoBehaviour
 
     private float move_force = 20f; // แรงที่ใช้ในการเดิน จะทดลองนำไปใช้คู่กับ Addforce ใน script นี้
     private int life = 10;
-    public float explode_force = 1000f;
+    // public float explode_force = 1000f;
     private bool is_dead;
+    public Transform Camera;
+    public float jump_force = 100f; // กำหนดแรงกระโดด
+    private bool is_grounded = true; // ตรวจสอบว่าลูกบอลอยู่บนพื้นหรือไม่
 
 
     // sound asset
@@ -39,6 +42,7 @@ public class BallScript : MonoBehaviour
         // life_text.text = life.ToString();
         // overscene.SetActive(false);
         // winnerscene.SetActive(false);
+
         
     }
 
@@ -55,17 +59,23 @@ public class BallScript : MonoBehaviour
         // }
         // Dead();
 
+        Move();
+        GetComponent<Rigidbody>().AddForce(Physics.gravity, ForceMode.Acceleration);
     }
 
     private void Move(){
 
-        //  ---- move part ----
+        // //  ---- move part ----
         float move_horizontal = Input.GetAxis("Horizontal");   //Horizontal แนวนอน
         float move_vertical = Input.GetAxis("Vertical");       // Vertical แนวตั้ง
         
         // กำหนดความเร็วของการเคลื่อนที่
-        Vector3 movement = new Vector3(move_horizontal, 0, move_vertical) * move_force;
-        // กำหนดความเร็วของการเคลื่อนที่เชิงเส้น (linear) ให้กับ Rigidbody
+        Vector3 forward = Camera.forward;
+        Vector3 right = Camera.right;
+
+        Vector3 movement = (forward * move_vertical + right * move_horizontal).normalized * move_force;
+        rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
+
         rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
 
         // move left
@@ -78,7 +88,28 @@ public class BallScript : MonoBehaviour
         {
             rb.AddForce(Vector3.right * move_force, ForceMode.Force);
         }
+
+        Jump();
     }
+
+    void Jump(){
+        if (Input.GetKeyDown(KeyCode.Space) && is_grounded)
+        {
+            rb.AddForce(Vector3.up * jump_force); // เพิ่มแรงกระโดด
+            is_grounded = false; // ตั้งค่าให้รู้ว่าลูกบอลอยู่กลางอากาศ
+
+            Debug.Log("Jump!");
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) // เช็คว่าลูกบอลแตะพื้น
+        {
+           is_grounded = true;
+           Debug.Log("Ground!");
+        }
+    }
+
 
     // check if dead
     // private void Dead(){
